efcf4e20cbd5a80bd89a8ccd4d69c5a588bfeda1
Bilella Works Dev <bilellaworks@gmail.com>
2026-09-13 14:34:49 +0100

    Initial Commit

---
+++ ridge v3/ridge_v3App.swift
@@ -0,0 +1,17 @@
+//
+//  ridge_v3App.swift
+//  ridge v3
+//
+//  Created by Matthew Bilella on 13/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct ridge_v3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
